b759bf530bd6d8a333421c369b8be894521359ff
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-14 01:26:41 +0000

    execution start

---
+++ mock.rb
@@ -0,0 +1,38 @@
+@players = [
+  {
+    player_id = player1,
+    name: name1,
+    life: life1,
+    answer: answer1,
+    valid: true || false,
+    turn: true || false,
+    number: number1,
+    score: score1,
+    life: life1,
+
+  },
+  {
+    player_id: player2,
+    name: name2,
+    life: life2,
+    answer: answer2,
+    valid: true || false,
+    turn: true || false,
+    number: number2,
+    score: score2,
+    life: life2,
+  }
+]
+
+@games = [
+  {
+    question_id: 1 (random(1-20)),
+    problem: problem1,
+    correct_answer: correct1,
+  },
+  {
+    question_id: 2,
+    problem: problem2,
+    correct_answer: correct2,
+  }
+]
---
+++ players.rb
@@ -0,0 +1,17 @@
+class Player
+
+  def player_name(name)
+    @player_name = gets.chomp
+  end
+
+  def player_life
+    @player_life === 3
+  end 
+
+  def player_answer(answer)
+    @player_answer = gets.chomp
+  end
+
+  
+  
+end
